456425792e11565773a2f2fc1069f18d2100bffb
tusharx0809 <tusharbhatia0809@outlook.com>
2024-04-28 18:00:03 -0400

    final-changes

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -9,13 +9,13 @@ def play_game(player_score, computer_score, ties):   #Here passing the values of
     comp_move = get_computer_move()
     if player_move == 'R' or player_move == 'r':
         if comp_move == 'p':
-            print('You Lose!, Computer chose Paper!')
+            print('You Lose! Computer chose Paper!')
             computer_score += 1
         elif comp_move == 's':
             print('You Win! Computer chose Scissor!')
             player_score += 1
         else:
-            print('It is a Tie!')
+            print('Computer chose Rock! It is a Tie!')
             ties += 1
     elif player_move == 'P' or player_move == 'p':
         if comp_move == 's':
@@ -25,7 +25,7 @@ def play_game(player_score, computer_score, ties):   #Here passing the values of
             print('You Win! Computer chose Rock!')
             player_score += 1
         else:
-            print("It is a Tie!")
+            print("Computer chose Paper! It is a Tie!")
             ties += 1
     elif player_move == 's' or player_move == 'S':
         if comp_move == 'r':
@@ -35,7 +35,7 @@ def play_game(player_score, computer_score, ties):   #Here passing the values of
             print('You Win! Computer chose Paper!')
             player_score += 1
         else:
-            print("It is a Tie!")
+            print("Computer chose Scissor! It is a Tie!")
             ties += 1
     return player_score, computer_score, ties #return these values so we can store them later to print
 def play_again():
@@ -55,5 +55,5 @@ print("Press R for rock, P for Paper and S for Scissor")
 while True:
     player_score, computer_score, ties = play_game(player_score, computer_score, ties) #call this function to store the value and keep a track of scores
     if not play_again(): #if player chooses not to play, then print the scores and break out of loop
-        print("Player:", player_score, "Computer:", computer_score, "Ties:", ties) 
+        print("Player Score:", player_score, "Computer Score:", computer_score, "Ties:", ties) 
         break
