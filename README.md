7adee72a4131ec39a1e738ce560d330c282fdc95
tusharx0809 <tusharbhatia0809@outlook.com>
2024-04-28 17:31:24 -0400

    created-play-game-function

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # basic-games
 # This is my first repository and to learn the basics of git commands I am starting with creating some small games in python.
-# first commit
+# first commit on my readme file
--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -1,6 +1,34 @@
 import random
 
-def computer_move():
+def get_computer_move():
     choices = ['r','p','s']
     return random.choice(choices)
 
+def play_game():
+    player_move = input(" --- PICK YOUR MOVE ---  ")
+    comp_move = get_computer_move()
+    if player_move == 'R' or player_move == 'r':
+        if comp_move == 'p':
+            print('You Lose!, Computer chose Paper!')
+        elif comp_move == 's':
+            print('You Win! Computer chose Scissor!')
+        else:
+            print('It is a Tie!')
+    elif player_move == 'P' or player_move == 'p':
+        if comp_move == 's':
+            print('You Lose! Computer chose Scissor!')
+        elif comp_move == 'r':
+            print('You Win! Computer chose Rock!')
+        else:
+            print("It is a Tie!")
+    elif player_move == 's' or player_move == 'S':
+        if comp_move == 'r':
+            print('You Lose! Computer chose Rock!')
+        elif comp_move == 'p':
+            print('You Win! Computer chose Paper!')
+        else:
+            print("It is a Tie!")
+
+print("Welcome to Rock Paper Scissor!")
+print("Press R for rock, P for Paper and S for Scissor")
+play_game()
