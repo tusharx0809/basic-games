242a8c20e78501573c5554f1994e7bba11bab76e
tusharx0809 <tusharbhatia0809@outlook.com>
2024-04-28 17:45:10 -0400

    added-score-functionality

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -4,30 +4,40 @@ def get_computer_move():
     choices = ['r','p','s']
     return random.choice(choices)
 
-def play_game():
+def play_game(player_score, computer_score, ties):   #Here passing the values of Player and computer scores
     player_move = input(" --- PICK YOUR MOVE ---  ")
     comp_move = get_computer_move()
     if player_move == 'R' or player_move == 'r':
         if comp_move == 'p':
             print('You Lose!, Computer chose Paper!')
+            computer_score += 1
         elif comp_move == 's':
             print('You Win! Computer chose Scissor!')
+            player_score += 1
         else:
             print('It is a Tie!')
+            ties += 1
     elif player_move == 'P' or player_move == 'p':
         if comp_move == 's':
             print('You Lose! Computer chose Scissor!')
+            computer_score += 1
         elif comp_move == 'r':
             print('You Win! Computer chose Rock!')
+            player_score += 1
         else:
             print("It is a Tie!")
+            ties += 1
     elif player_move == 's' or player_move == 'S':
         if comp_move == 'r':
             print('You Lose! Computer chose Rock!')
+            computer_score += 1
         elif comp_move == 'p':
             print('You Win! Computer chose Paper!')
+            player_score += 1
         else:
             print("It is a Tie!")
+            ties += 1
+    return player_score, computer_score, ties #return these values so we can store them later to print
 def play_again():
     print("Do you want to Play again?(Y/N)")
     answer = input("Press Y for Yes and N for No: ")
@@ -36,10 +46,14 @@ def play_again():
     else:
         return False
     
+player_score = 0
+computer_score = 0
+ties = 0
 print("Welcome to Rock Paper Scissor!")
 print("Press R for rock, P for Paper and S for Scissor")
 
 while True:
-    play_game()
-    if not play_again():
+    player_score, computer_score, ties = play_game(player_score, computer_score, ties) #call this function to store the value and keep a track of scores
+    if not play_again(): #if player chooses not to play, then print the scores and break out of loop
+        print("Player:", player_score, "Computer:", computer_score, "Ties:", ties) 
         break
